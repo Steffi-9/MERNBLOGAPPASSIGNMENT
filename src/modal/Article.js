7d22533675f3e69ffbf3166241ae51c54dd52360
Steffi A Pereira <steffi@softviz.com>
2022-02-10 08:31:00 +0400

    commit

---
+++ src/modal/Article.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+let mongooseSchema = mongoose.Schema;
+const articleSchema = new mongooseSchema({
+    title:String,
+    description:String,
+    comment:String,
+    
+});
+
+var articleModelObj = mongoose.model("Articles",articleSchema);
+module.exports = {articleModelObj};
